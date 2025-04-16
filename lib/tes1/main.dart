a1ddb006f2dd69bf2146a59d0316651db8572a9d
rwankurnia <rwankurnia@gmail.com>
2025-04-16 12:20:52 +0700

    tes bae

--- lib/tes1/main.dart
+++ lib/tes1/main.dart
@@ -113,5 +113,6 @@ void hello2(String name, int age) {
 }
 // Named parameters
 void hello3({required String name, int? age}) {
-  print('Hello, namaku $name dan umurku ${age ?? 'rahasia'}');
+  print('Hello, namaku $name dan umurku ${age ?? 'rahasia donk'}');
 }
+
